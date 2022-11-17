6147af3281e84fa2662952b7a9b7816ac0be81f6
kothiyayogesh11 <kothiyayogesh11@gmail.com>
2022-11-17 10:45:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,47 +2,47 @@ import os
 class Config(object):
     DEBUG = True
     TESTING = True
-    DB_NAME="spafest-dev"
+    DB_NAME="miiot-dev"
     SECRET_KEY = "B\xb2?.\xdf\x9f\xa7m\xf8\x8a%,\xf7\xc4\xfa\x91"
-    MONGO_URI="mongodb+srv://itech:itech%40123@cluster0-wbh2p.mongodb.net/test?retryWrites=true&w=majority"
+    MONGO_URI="YOUR_CONNECTION_URL"
     IMAGE_UPLOADS = "/home/username/projects/my_app/app/static/images/uploads"
     SESSION_COOKIE_SECURE = False
-    SMS_SERVER = "http://ui.netsms.co.in/"
-    SMS_USER = "iTechNotion"
-    SMS_PASSWORD = "itech@006"
-    STATIC_TOKEN = "5uEDbB9P97DLyMWV"
+    SMS_SERVER = ""
+    SMS_USER = ""
+    SMS_PASSWORD = ""
+    STATIC_TOKEN = ""
     
 
 class ProductionConfig(Config):
     DEBUG = False
-    MONGO_URI="mongodb+srv://itech:itech%40123@cluster0-wbh2p.mongodb.net/test?retryWrites=true&w=majority"
-    DB_NAME="wellnessta-dev"
+    MONGO_URI="YOUR_CONNECTION_URL"
+    DB_NAME="miiot-dev"
     IMAGE_UPLOADS = "/home/username/projects/my_app/app/static/images/uploads"
     SESSION_COOKIE_SECURE = True
-    SMS_SERVER : "http://ui.netsms.co.in/"
-    SMS_USER : "iTechNotion"
-    SMS_PASSWORD : "itech@006"
-    STATIC_TOKEN = "5uEDbB9P97DLyMWV"
+    SMS_SERVER : ""
+    SMS_USER : ""
+    SMS_PASSWORD : ""
+    STATIC_TOKEN = ""
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    MONGO_URI="mongodb+srv://itech:itech%40123@cluster0-wbh2p.mongodb.net/test?retryWrites=true&w=majority"
-    DB_NAME="spafest-dev"
+    MONGO_URI="YOUR_CONNECTION_URL"
+    DB_NAME="miiot-dev"
     IMAGE_UPLOADS = "/home/username/projects/my_app/app/static/images/uploads"
     SESSION_COOKIE_SECURE = False
-    SMS_SERVER : "http://ui.netsms.co.in/"
-    SMS_USER : "iTechNotion"
-    SMS_PASSWORD : "itech@006"
-    STATIC_TOKEN = "5uEDbB9P97DLyMWV"
+    SMS_SERVER : ""
+    SMS_USER : ""
+    SMS_PASSWORD : ""
+    STATIC_TOKEN = ""
 
 class TestingConfig(Config):
     DEBUG = True
     TESTING = True
-    MONGO_URI="mongodb+srv://itech:itech%40123@cluster0-wbh2p.mongodb.net/test?retryWrites=true&w=majority"
-    DB_NAME="spafest-dev"
+    MONGO_URI="YOUR_CONNECTION_URL"
+    DB_NAME="miiot-dev"
     IMAGE_UPLOADS = "/home/username/projects/my_app/app/static/images/uploads"
     SESSION_COOKIE_SECURE = False
-    SMS_SERVER : "http://ui.netsms.co.in/"
-    SMS_USER : "iTechNotion"
-    SMS_PASSWORD : "itech@006"
-    STATIC_TOKEN = "5uEDbB9P97DLyMWV"    
+    SMS_SERVER : ""
+    SMS_USER : ""
+    SMS_PASSWORD : ""
+    STATIC_TOKEN = ""    
